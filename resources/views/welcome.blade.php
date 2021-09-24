540de2d7b40eb7b6981f5b9f2701dbaa8bc54c84
Wagner Castro <wagner.castro.m2center@gmail.com>
2021-09-24 07:53:29 -0300

    #03 Update Laravel 5.5 to 5.8 and install laravel/ui vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,24 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-    <div class="flex-center position-ref full-height">
-        <example></example>
-    </div>
-@endsection
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+    <body>
+        <div id="app" class="flex-center position-ref full-height">
+            <example-component></example-component>
+        </div>
+        <script type="text/javascript" src="{{ asset('js/manifest.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/vendor.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
